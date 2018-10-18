c27b1b5aeb1f28011e08c46231583570bd69906b
Chris Reid <chris.reid@akqa.com>
2018-10-18 13:52:35 +0100

    Redux store initial commit

--- src/app/state/store.js
+++ src/app/state/store.js
@@ -2,8 +2,11 @@ import {createStore, applyMiddleware, combineReducers} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
+import routesReducer from './models/routes/reducers'
+
 const reducer = combineReducers({
-});
+  routes: routesReducer,
+})
 
 const store = createStore(
   reducer,
--- src/app/state/store.test.js
+++ src/app/state/store.test.js
@@ -4,9 +4,12 @@ describe('store', () => {
 
   it('should initialise', () => {
     const actual = store.getState()
-    const expected = {}
-
-    console.log(actual)
+    const expected = {
+      routes: {
+        isFetching: false,
+          routes: []
+      }
+    }
 
     expect(actual).toEqual(expected)
   })
